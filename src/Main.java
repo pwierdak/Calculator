906f1a30b549453fddac3be3c4322b52a2a204f8
pwierdak <paulinawierdak95@gmail.com>
2022-09-21 18:16:01 +0200

    Case 2, 3, 4, Result

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello! What do you want to do?\n1.addition\n2.substraction\n3.multiplication\n4.division");
+        System.out.println("Hello! What do you want to do?\nSelect:\n1.addition\n2.substraction\n3.multiplication\n4.division");
         Scanner scanner=new Scanner(System.in);
         System.out.println("Please, enter first value.");
         int x= scanner.nextInt();
@@ -11,8 +11,17 @@ public class Main {
         int select= scanner.nextInt();
         switch (select){
             case 1:
-                System.out.println(x+y);
+                System.out.println("result" + (x+y));
                 break;
+            case 2:
+                System.out.println("result" + (x-y));
+                break;
+            case 3:
+                System.out.println("result" + (x*y));
+                break;
+            case 4:
+                System.out.println("result" + (x/y));
+
         }
     }
 }
